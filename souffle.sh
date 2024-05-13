85eceeaaffd8bf4b3be91539f9afd760eab88057
stuuupidcat <stuuupidcat@163.com>
2024-05-14 00:25:27 +0800

    Update souffle.sh

--- souffle.sh
+++ souffle.sh
@@ -1,1 +1,3 @@
-rm -rf ./Rust/CVE-2020-35906/output/* && souffle -F ./Rust/CVE-2020-35906/facts -D ./Rust/CVE-2020-35906/output ./Rust/CVE-2020-35906/CVE-2020-35906.dl 
+rm -rf ./Rust/CVE-2020-35906/output/ && mkdir -p ./Rust/CVE-2020-35906/output/ && souffle -F ./Rust/CVE-2020-35906/facts -D ./Rust/CVE-2020-35906/output ./Rust/CVE-2020-35906/CVE-2020-35906.dl
+
+rm -rf ./Rust/CVE-2018-21000/output/ && mkdir -p ./Rust/CVE-2018-21000/output/ && souffle -F ./Rust/CVE-2018-21000/facts -D ./Rust/CVE-2018-21000/output ./Rust/CVE-2018-21000/CVE-2018-21000.dl
